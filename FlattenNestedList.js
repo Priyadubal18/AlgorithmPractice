0b9fd3cb9b88a1cee71e5e3b34890c2f866ec121
Priyadubal18 <priya.dubal18@gmai.com>
2019-11-23 13:55:29 -0500

    Merge two Binary Tree

--- FlattenNestedList.js
+++ FlattenNestedList.js
@@ -1,4 +1,4 @@
-/**
+/**https://leetcode.com/problems/flatten-nested-list-iterator/
  * // This is the interface that allows for creating nested lists.
  * // You should not implement it, or speculate about its implementation
  * function NestedInteger() {
